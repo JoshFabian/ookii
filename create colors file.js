7390e3f207c3eac8028653879dd7606cc13ae523
Jonathan Gertig <jcgertig@gmail.com>
2016-09-12 00:49:14 -0500

    remove unneeded

---
+++ create colors file.js
@@ -0,0 +1,17 @@
+// COLORS = GAME.make.bitmapData(32 * 51, 32);
+// TAILS = GAME.add.bitmapData(32 * 51, 32);
+// const colors = Phaser.Color.HSVColorWheel(); //eslint-disable-line babel/new-cap
+//
+// // const white = Phaser.Color.createColor(255, 255, 255).rgba;
+// const black = Phaser.Color.createColor(1, 1, 1).rgba;
+// COLORS.rect(2, 2, 28, 28, black);
+// TAILS.rect(2, 2, 28, 28, colors[0].rgba);
+// for (let x = 1; x < 51; x++) {
+//   const color = colors[x * 6].rgba;
+//   COLORS.rect(x * 32, 0, 32, 32, color);
+//
+//   TAILS.rect((x * 32) + 2, 2, 28, 28, black);
+//   TAILS.rect((x * 32) + 6, 0, 20, 32, colors[x * 6].rgba);
+// }
+//
+// downloadCanvasAsImage(COLORS.ctx.canvas);
